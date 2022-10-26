c33fa0d66b4000b7c50b78993f6573398eaad7e9
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-10-26 11:12:06 +0800

    Update to v22.03.0

--- build_image.py
+++ build_image.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from urllib.request import urlretrieve
 from urllib.parse import urlparse
 
-VERSION = "22.03.0-rc6"
+VERSION = "22.03.0"
 TARGET = "rockchip"
 SUBTARGET = "armv8"
 PROFILE = "friendlyarm_nanopi-r4s"
