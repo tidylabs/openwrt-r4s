724b49c488e54e6d95589fc106f9e8ff4603afd6
Justin Klaassen <6700251+jck112@users.noreply.github.com>
2022-10-26 11:12:06 +0800

    Add build_image.py

---
+++ build_image.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import subprocess
+import tarfile
+
+from pathlib import Path
+from urllib.request import urlretrieve
+from urllib.parse import urlparse
+
+VERSION = "22.03.0-rc5"
+TARGET = "rockchip"
+SUBTARGET = "armv8"
+PROFILE = "friendlyarm_nanopi-r4s"
+PACKAGES = [
+    "firewall", "ip6tables-legacy", "iptables-legacy", "kmod-ipt-offload",
+    "luci-ssl", "-firewall4", "-nftables", "-kmod-nft-offload"
+]
+EXTRA_PACKAGES = [
+    "https://downloads.openwrt.org/releases/22.03-SNAPSHOT/targets/rockchip/armv8/packages/libwolfssl5.4.0.ee39414e_5.4.0-stable-3_aarch64_generic.ipk",
+]
+
+URL_IMAGEBUILDER = f"https://downloads.openwrt.org/releases/{VERSION}/targets/{TARGET}/{SUBTARGET}/openwrt-imagebuilder-{VERSION}-{TARGET}-{SUBTARGET}.Linux-x86_64.tar.xz"
+
+
+def urldownload(url, path="."):
+    abs_path = os.path.abspath(path)
+    if not os.path.isdir(abs_path):
+        os.makedirs(abs_path)
+
+    url_path = urlparse(url).path
+    filename = os.path.basename(url_path)
+    out_path = os.path.join(abs_path, filename)
+
+    print(f"Downloading {filename}")
+    return urlretrieve(url, out_path)[0]
+
+
+if __name__ == "__main__":
+    imagebuilder_tar = urldownload(URL_IMAGEBUILDER)
+    with tarfile.open(name=imagebuilder_tar) as tar_file:
+        tar_file.extractall()
+
+    os.chdir(
+        f"openwrt-imagebuilder-{VERSION}-{TARGET}-{SUBTARGET}.Linux-x86_64")
+
+    for package_url in EXTRA_PACKAGES:
+        urldownload(package_url, "./packages/")
+
+    subprocess.run([
+        "make", "image", f"PROFILE={PROFILE}", f"PACKAGES={' '.join(PACKAGES)}"
+    ]).check_returncode()
+
+    bin_dir = Path(f"./bin/targets/{TARGET}/{SUBTARGET}/")
+    for image_file in bin_dir.glob(f"openwrt-{VERSION}-{TARGET}-{SUBTARGET}-{PROFILE}-*-sysupgrade.img.gz"):
+        print(f"Copying {image_file.name}")
+        shutil.copy(image_file, "..")
+
